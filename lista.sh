775820909e21de55285266e6b764dfe311e89bb9
schuhpereira <eduardopereira@ienh.com.br>
2016-08-15 20:25:25 -0300

    Update lista.sh

--- lista.sh
+++ lista.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 # Diretórios que serão salvos os arquivos de anotações
-DIR="/home/eduardo/Arquivos/Dropbox/IENH/2016_2/STI_segunda/"
-DIR2="/home/eduardo/Arquivos/Dropbox/IENH/2016_2/RLD_terca/"
-DIR3="/home/eduardo/Arquivos/Dropbox/IENH/2016_2/ADM2_quarta"
-DIR4="/home/eduardo/Arquivos/Dropbox/IENH/2016_2/PR2_quinta"
-DIR5="/home/eduardo/Arquivos/Dropbox/IENH/2016_2/SRI_sexta"
-DIR6="/home/eduardo/Arquivos/Dropbox/IENH/2016_2/EstagioUm_quarta"
-DIR7="/home/eduardo/Arquivos/Dropbox/IENH/2016_2/EstagioDois_quarta"
-
+DIR="/home/eduardo/Arquivos/Dropbox/IENH/2016_2"
 
 # Lista os arquivos nos diretórios citados acima
-find $DIR $DIR2 $DIR3 $DIR4 $DIR5 $DIR6 $DIR7 -name "*.txt" > listaArquivos.txt 
-	yad --text-info --title="Arquivos Recentes" \
-		--width=500 --height=400 \
-		--filename="listaArquivos.txt" --editable --show-uri --wrap \
+#find $DIR -name "*.txt" > listaArquivos.txt 
+
+for d in $DIR do
+
+find $d -type f
+yad --text-info --title="Arquivos Recentes" \
+		--width=600 --height=300 \
+		--filename="listaArquivos.txt" \
 		--fore="black" --back="#AAFFAA" --fontname="Ubuntu Mono 12" \
 		--justify="fill" --margins="10" \
 		--show-uri \
 		--button='Sair':1 --button='Abrir':0
+done
